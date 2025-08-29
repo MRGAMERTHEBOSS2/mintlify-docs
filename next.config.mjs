084a284fd3ce79f33c307e1c9403ab9f17c887de
VxTrustedTyler <tylergayle16@gmail.com>
2025-08-29 11:50:48 -0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import nextra from 'nextra';
+
+const withNextra = nextra({
+theme: 'nextra-theme-docs',
+themeConfig: '.https://www.google.com/search?q=/theme.config.jsx',
+defaultShowCopyCode: true
+});
+
+export default withNextra();
